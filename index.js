63ceadd23e30b7a61ee0f9b3b99573d6a5cf5d77
Akpur Bibiana <akpurbibi@gmail.com>
2025-03-06 10:03:47 +0100

    first javascript code

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("innercontainer").innerHTML="BEAULAH CHILDREN HOME";
+    document.getElementById("content_container").style.backgroundColor = "blue";
+};
+function alertMe() {
+    window.alert("welcome to my page");
+}
